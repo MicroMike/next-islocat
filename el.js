3c0164fca85750c29715087eb500184ca76fd3d5
Your Name <you@example.com>
2018-05-31 21:50:31 +0200

    'fdgf'

--- el.js
+++ el.js
@@ -5,8 +5,8 @@ const nightmare = Nightmare({
   // openDevTools: {
   //   mode: 'detach'
   // },
-  waitTimeout: 3600000,
-  show: true,
+  waitTimeout: 60000,
+  show: false,
   typeInterval: 300,
   webPreferences: {
     webSecurity: false,
@@ -51,6 +51,93 @@ var emails = [
   'belal.khang@0live.org',
   'keontre.toki@0live.org',
   'jontrell.darcy@0live.org',
+  'ermias.paris@0ils.net',
+  'aasim.oluwatomiwa@ldaho.net',
+  'frederic.raihan@ldaho.net',
+  'kensington.alvis@ldaho.net',
+  'marlow.novah@ldaho.net',
+  'reynold.donnie@ldaho.net',
+  'marlow.novah@ldaho.net',
+  'reynold.donnie@ldaho.net',
+  'jahkye.osbourne@ldaho.net',
+  'kayse.zhaire@ldaho.net',
+  'taran.gardner@ldaho.net',
+  'akari.kohl@ldaho.net',
+  'imaan.mattox@ldaho.net',
+  'akari.kohl@ldaho.net',
+  'kayse.zhaire@ldaho.net',
+  'imaan.mattox@ldaho.net',
+  'taran.gardner@ldaho.net',
+  'roland.avondre@ldaho.net',
+  'master.mantra@ldaho.net',
+  'ephriam.azarias@ldaho.net',
+  'chelsea.kennett@ldaho.net',
+  'huzaifa.brendyn@ldaho.net',
+  'master.mantra@ldaho.net',
+  'huzaifa.brendyn@ldaho.net',
+  'ephriam.azarias@ldaho.net',
+  'roland.avondre@ldaho.net',
+  'elder.suyash@ldaho.net',
+  'sofian.emari@ldaho.net',
+  'tyris.phoenixx@ldaho.net',
+  'reif.jaiceon@ldaho.net',
+  'elder.suyash@ldaho.net',
+  'rayon.trek@ldaho.net',
+  'reif.jaiceon@ldaho.net',
+  'tyris.phoenixx@ldaho.net',
+  'sofian.emari@ldaho.net',
+  'brenner.hailey@ldaho.net',
+  'rally.zailyn@ldaho.net',
+  'rally.zailyn@ldaho.net',
+  'qian.artavious@ldaho.net',
+  'justin.kees@ldaho.net',
+  'brenner.hailey@ldaho.net',
+  'justin.kees@ldaho.net',
+  'qian.artavious@ldaho.net',
+  'yeshaya.abraham@ldaho.net',
+  'julien.chadrick@ldaho.net',
+  'mercy.aalijah@ldaho.net',
+  'kaos.ulices@ldaho.net',
+  'brodrick.draven@ldaho.net',
+  'julien.chadrick@ldaho.net',
+  'yeshaya.abraham@ldaho.net',
+  'mercy.aalijah@ldaho.net',
+  'brodrick.draven@ldaho.net',
+  'brentyn.manav@ldaho.net',
+  'zebulon.preslee@ldaho.net',
+  'lukah.samik@ldaho.net',
+  'rayne.arin@ldaho.net',
+  'riker.oleg@ldaho.net',
+  'zebulon.preslee@ldaho.net',
+  'riker.oleg@ldaho.net',
+  'rayne.arin@ldaho.net',
+  'lukah.samik@ldaho.net',
+  'damiano.bob@ldaho.net',
+  'mizael.lofton@ldaho.net',
+  'cort.tenzing@ldaho.net',
+  'damiano.bob@ldaho.net',
+  'angelito.harfateh@ldaho.net',
+  'mizael.lofton@ldaho.net',
+  'eyoel.emin@ldaho.net',
+  'angelito.harfateh@ldaho.net',
+  'eyoel.emin@ldaho.net',
+  'wheeler.niklas@ldaho.net',
+  'muhammad.severiano@ldaho.net',
+  'rajveer.prynceton@ldaho.net',
+  'syair.kengo@ldaho.net',
+  'wheeler.niklas@ldaho.net',
+  'muhammad.severiano@ldaho.net',
+  'rajveer.prynceton@ldaho.net',
+  'richy.aun@ldaho.net',
+  'nolen.haston@ldaho.net',
+  'brantson.degan@ldaho.net',
+  'richy.aun@ldaho.net',
+  'brantson.degan@ldaho.net',
+  'jotaro.xiomar@ldaho.net',
+  'nolen.haston@ldaho.net',
+  'russell.kyriee@ldaho.net',
+  'jotaro.xiomar@ldaho.net',
+  'russell.kyriee@ldaho.net',
 ]
 
 function getRandomInt(max, min) {
@@ -78,57 +165,41 @@ var intervalHours = getRandomInt(oneHour * 3, oneHour * 1.5)
 
 var yn70 = () => (getRandomInt(10, 1) > 7 ? true : false)
 
-const doItAgain = (first) => {
-  console.log(first ? 'start' : 'change')
-
-  nightmare
+const doItAgain = async (first) => {
+  await nightmare
+    .wait(5000)
     .goto('https://open.spotify.com/' + artists[getRandomInt(artists.length, 0)])
-    .wait(3000)
     .forward()
     // .click('.artist-header .btn.btn-black')
+    .wait('.tracklist-play-pause.tracklist-middle-align')
     .click('.tracklist-play-pause.tracklist-middle-align')
     .wait(5000)
     .click('.control-button.spoticon-shuffle-16')
+
+  await console.log('\x1b[34m%s\x1b[0m', first ? 'start' : 'change')
 }
 
-const tempmail = [
+const tempmaillist = [
   'https://www.tempmailaddress.com',
   'https://www.tempmailaddress.com',
   // 'https://www.mohmal.com/fr/create/random',
   // 'https://www.crazymailing.com',
 ]
 
-var emailurl = tempmail[getRandomInt(tempmail.length, 0)]
+var emailurl = tempmaillist[getRandomInt(tempmaillist.length, 0)]
 
-const create = async (newAccount, captcha) => {
+const create = async (newAccount, captcha, tempmail) => {
   try {
-    const tempmail = newAccount
-      ? await nightmare
-        .goto(emailurl)
-        .wait(2000)
-        .evaluate((emailurl) => {
-          switch (emailurl) {
-            case 'https://www.mohmal.com/fr/create/random':
-              return $('[data-email]').attr('data-email')
-            case 'https://www.crazymailing.com':
-              return document.getElementById('email_addr').innerText
-            case 'https://www.tempmailaddress.com':
-              return document.getElementById('email').innerText
-          }
-        }, emailurl)
-      : emails[getRandomInt(emails.length, 0)]
-
-    console.log('load: ' + tempmail)
-
-    nightmare
+    await nightmare
       .goto(url(newAccount))
       .forward()
-      .evaluate((captcha) => {
-        console.log(captcha)
-        document.getElementById('g-recaptcha-response').value = captcha
-      }, captcha)
 
     if (newAccount) {
+      await nightmare
+        .evaluate((captcha) => {
+          document.getElementById('g-recaptcha-response').value = captcha
+        }, captcha)
+
       nightmare
         .type('form input[name="email"]', tempmail)
         .type('form input[name="confirm_email"]', tempmail)
@@ -138,22 +209,24 @@ const create = async (newAccount, captcha) => {
         .select('form select[name="dob_month"]', month)
         .type('form input[name="dob_year"]', getRandomInt(32, 1963))
         .click('form input[id="register-male"]')
+        .wait(10000)
+        .click('#register-button-email-submit')
     }
     else {
-      nightmare
+      await nightmare
         .type('form input[name="username"]', tempmail)
         .type('form input[name="password"]', tempmail)
+        .wait(2000)
+        .evaluate((captcha) => {
+          window.___grecaptcha_cfg.clients[0].ba.l.callback(captcha)
+        }, captcha)
     }
 
-    await nightmare
-      .click(newAccount ? '#register-button-email-submit' : '#login-button')
-      .wait(10000)
-      .wait('.welcome-message')
-
-    await console.log((newAccount ? 'account created: ' : 'account logged: ') + tempmail)
+    await console.log('\x1b[32m%s\x1b[0m', (newAccount ? 'account created: ' : 'account logged: ') + tempmail)
 
     if (newAccount) {
       await nightmare
+        .wait('.welcome-message')
         .goto(emailurl)
         .forward()
 
@@ -192,10 +265,10 @@ const create = async (newAccount, captcha) => {
         .wait(5000)
     }
 
-    // doItAgain(true)
+    doItAgain(true)
   }
   catch (e) {
-    console.log(e)
+    console.log('\x1b[31m%s\x1b[0m', e)
   }
 
   setInterval(() => {
@@ -210,23 +283,12 @@ const create = async (newAccount, captcha) => {
 
   nightmare
     .catch(error => {
-      console.error('Search failed:', error)
+      console.error('\x1b[31m%s\x1b[0m', 'Search failed:', error)
     })
 
 }
 
-// setTimeout(() => {
-//   create(true)
-// }, getRandomInt(120000));
-
-// setInterval(() => {
-//   create(true)
-//   // create(yn70)
-// }, intervalHours)
-
-var newAccount = true
-
-const twocaptcha = () => {
+const twocaptcha = (newAccount, tempmail) => {
   request.post({
     url: 'http://2captcha.com/in.php',
     data: {
@@ -234,6 +296,7 @@ const twocaptcha = () => {
       method: 'userrecaptcha',
       googlekey: newAccount ? '6LdaGwcTAAAAAJfb0xQdr3FqU4ZzfAc_QZvIPby5' : '6LeIZkQUAAAAANoHuYD1qz5bV_ANGCJ7n7OAW3mo',
       pageurl: newAccount ? 'https://spotify.com/fr/signup' : 'https://accounts.spotify.com/fr/login',
+      invisible: newAccount ? 0 : 1
     }
   }, function (err, res, body) {
     console.log(body)
@@ -251,7 +314,7 @@ const twocaptcha = () => {
 
         if (body !== 'CAPCHA_NOT_READY') {
           clearInterval(interval)
-          create(newAccount, body.split('|')[1])
+          create(newAccount, body.split('|')[1], tempmail)
         }
         else {
           console.log(body.split('|')[0])
@@ -261,7 +324,7 @@ const twocaptcha = () => {
   })
 }
 
-const anticaptcha = () => {
+const anticaptcha = (newAccount, tempmail) => {
   request({
     url: 'https://api.anti-captcha.com/createTask',
     method: 'POST',
@@ -272,6 +335,7 @@ const anticaptcha = () => {
         type: 'NoCaptchaTaskProxyless',
         websiteKey: newAccount ? '6LdaGwcTAAAAAJfb0xQdr3FqU4ZzfAc_QZvIPby5' : '6LeIZkQUAAAAANoHuYD1qz5bV_ANGCJ7n7OAW3mo',
         websiteURL: newAccount ? 'https://spotify.com/fr/signup' : 'https://accounts.spotify.com/fr/login',
+        invisible: newAccount ? 0 : 1
       }
     }
   }, function (err, res, response) {
@@ -289,7 +353,7 @@ const anticaptcha = () => {
       }, function (err, res, response) {
         if (response.status !== 'processing') {
           clearInterval(interval)
-          create(newAccount, response.solution.gRecaptchaResponse)
+          create(newAccount, response.solution.gRecaptchaResponse, tempmail)
         }
         else {
           console.log(response)
@@ -299,5 +363,28 @@ const anticaptcha = () => {
   });
 }
 
-anticaptcha();
-// create(true)
+const isNew = process.env.NEWACCOUNT === '1'
+
+const tempmail = isNew
+  ? nightmare
+    .goto(emailurl)
+    .wait(2000)
+    .evaluate((emailurl) => {
+      switch (emailurl) {
+        case 'https://www.mohmal.com/fr/create/random':
+          return $('[data-email]').attr('data-email')
+        case 'https://www.crazymailing.com':
+          return document.getElementById('email_addr').innerText
+        case 'https://www.tempmailaddress.com':
+          return document.getElementById('email').innerText
+      }
+    }, emailurl)
+  : emails[getRandomInt(emails.length, 0)]
+
+console.log('\x1b[33m%s\x1b[0m', 'load: ' + tempmail)
+
+setTimeout(async () => {
+  anticaptcha(isNew, tempmail);
+  // twocaptcha(isNew);
+  // create(true)
+}, getRandomInt(180000));
